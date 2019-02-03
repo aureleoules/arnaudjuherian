03dbfa1c7de1e1312058504602bf55a7a3995836
Aurèle Oulès <aurele@oules.com>
2019-02-03 20:06:18 +0100

    reduced cache

--- src/services/client.js
+++ src/services/client.js
@@ -28,7 +28,7 @@ async function configure() {
 
         // `axios-cache-adapter` options
         cache: process.env.NODE_ENV === "development" ? null : {
-            maxAge: 5 * 60 * 1000, //5mins
+            maxAge: 30 * 1000, //5mins
             store // Pass `localforage` store to `axios-cache-adapter`
         },
     })
